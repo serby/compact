70a6389970d3f2f2696392ef37a49605f3d73e46
Paul Serby <paul.serby@clock.co.uk>
2012-02-05 22:57:08 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# compact.js - A simple JavaScript and CSS compacting middleware for express
+
+## Installation
+
+      npm install compact
+
+## Usage
+
+```js
+
+var compact = require('compact').createCompact(
+__dirname + '/public/src/',
+__dirname + '/public/compact/',
+  '/js/compact/'
+);
+
+compact.addNamespace('global');
+
+compact.ns.global
+	.addJs('/js/main.js')
+	.addJs('/js/widget-a.js')
+	.addJs('/js/widget-b.js');
+
+compact.addNamespace('home')
+	.addJs('/js/banner.js')
+	.addJs('/js/ads.js');
+
+compact.addNamespace('profile')
+	.addJs('/js/profile.js');
+
+compact.addNamespace('comments')
+	.addJs('/js/paging.js');
+	.addJs('/js/comments.js');
+
+// All routes will have global
+app.use(compact.js(['global']))
+
+// Add some compacted JavaScript for just this route. Having the namespaces
+// in separate arrays will produce a javascript file per array.
+app.get('/', compact.js(['home'], ['profile']));
+
+// Having different namespaces joined together will combine and output as one
+// javascript file.
+app.get('/blog', compact.js(['comments', 'profile']));
+
+```
+
+Then in the view use the **compactJsHtml()** view helper
+
+```html
+#{compactJsHtml()}
+```
+On / you'd get the following
+
+```html
+<script src="/js/compact/global.js"></script>
+<script src="/js/compact/home.js"></script>
+<script src="/js/compact/profile.js"></script>
+```
+
+On /blog you'd get this
+
+```html
+<script src="/js/compact/global.js"></script>
+<script src="/js/compact/comment-profile.js"></script>
+```
+
+You also have access to the **compactJs()** helper which will return an array
+of files for you to include on the page.
+
+## Credits
+[Paul Serby](https://github.com/serby/)
+
+## Licence
+Licenced under the [New BSD License](http://opensource.org/licenses/bsd-license.php)
---
+++ test/compact.test.js
@@ -0,0 +1,192 @@
+var fs = require('fs')
+  , mkdirp = require('mkdirp')
+  , async = require('asyncjs');
+
+
+var srcPath = __dirname + '/assets'
+  , destPath = __dirname + '/tmp';
+
+function createFiles(done) {
+  mkdirp(destPath, done);
+}
+
+function removeFiles(done) {
+  async.rmtree(destPath, done);
+}
+
+describe('compact.js', function() {
+
+  beforeEach(createFiles);
+  afterEach(removeFiles);
+
+  describe('#createCompact()', function() {
+
+    it('should error with invalid source path', function() {
+      (function() {
+        var compact = require('../../compact').createCompact('invalid src path');
+      }).should.throw('Invalid source path \'invalid src path\'');
+    });
+
+    it('should error with invalid destination path', function() {
+      (function() {
+        var compact = require('../../compact').createCompact(srcPath, 'invalid dest path');
+      }).should.throw('Invalid destination path \'invalid dest path\'');
+    });
+
+    it('should succeed with valid paths', function() {
+      require('../../compact')
+        .createCompact(srcPath, destPath).should.be.a('object');
+    });
+  });
+
+  describe('#addNamespace()', function() {
+    var compact;
+
+    beforeEach(function() {
+      compact = require('../../compact').createCompact(srcPath, destPath);
+    });
+
+    it('should fail on null', function() {
+      (function() {
+        compact.addNamespace(null);
+      }).should.throw('Invalid namespace');
+    });
+
+    it('should succeed with valid namespace', function() {
+      compact.addNamespace('global').addJs.should.be.a('function');
+    });
+  });
+
+  describe('Namespace', function() {
+
+    var namespace
+      , compact;
+
+    beforeEach(function() {
+      compact = require('../../compact').createCompact(srcPath, destPath);
+      namespace = compact.addNamespace('global');
+    });
+
+    describe('access via .ns', function() {
+      it('should be accessible via ', function() {
+        compact.ns.global.should = Object.prototype.should;
+        compact.ns.global.should.be.a('object');
+      });
+
+      it('should not be able to mess with a namespace', function() {
+        compact.ns.global.should = Object.prototype.should;
+        (function() {
+          compact.ns.global = {};
+        }).should.throw('You can not alter a registered namespace \'global\'');
+
+      });
+    });
+
+    describe('#addJs()', function() {
+
+      it('should succeed with valid file', function() {
+        namespace.addJs('a.js');
+      });
+
+      it('should be chainable', function() {
+        namespace
+          .addJs('a.js')
+          .addJs('b.js');
+      });
+
+      it('should be able to add via .ns', function() {
+        compact.ns.global
+          .addJs('a.js')
+          .addJs('b.js');
+      });
+    });
+  });
+
+  describe('#js()', function() {
+
+    var namespace
+      , compact;
+
+    beforeEach(function() {
+      compact = require('../../compact').createCompact(srcPath, destPath);
+    });
+
+    it('should error without parameter', function() {
+      (function() {
+        compact.js();
+      }).should.throw('You must pass one or more arrays containing valid namespace names');
+
+    });
+
+    it('should succeed with empty array as first parameter', function() {
+      compact.js([]).should.be.a('function');
+    });
+
+    it('should create a helper when given valid input for a single namespace', function(done) {
+      compact.addNamespace('global')
+      .addJs('/a.js')
+      .addJs('/b.js');
+
+      var
+        req = {
+          app: {
+            helpers: function(helper) {
+              helper.compactJs().should.eql(['/global.js']);
+              done();
+            }
+          }
+        }
+        , res;
+
+      compact.js(['global'])(req, res, function() {});
+    });
+
+    it('should have a correct helper when given valid input for multiple namespaces', function(done) {
+
+      compact.addNamespace('global')
+        .addJs('/a.js')
+        .addJs('/b.js');
+
+        compact.addNamespace('profile')
+        .addJs('/c.js');
+
+      var
+        req = {
+          app: {
+            helpers: function(helper) {
+              helper.compactJs().should.eql(['/global-profile.js']);
+              done();
+            }
+          }
+        }
+        , res;
+
+      compact.js(['global', 'profile'])(req, res, function() {});
+    });
+
+    it('should have a correct helper when given valid input for multiple groups', function(done) {
+
+      compact.addNamespace('global')
+        .addJs('/a.js');
+
+      compact.addNamespace('blog')
+        .addJs('/b.js');
+
+        compact.addNamespace('profile')
+        .addJs('/c.js');
+
+      var
+        req = {
+          app: {
+            helpers: function(helper) {
+              helper.compactJs().should.eql(['/global-profile.js', '/blog.js']);
+              done();
+            }
+          }
+        }
+        , res;
+
+      compact.js(['global', 'profile'], ['blog'])(req, res, function() {});
+    });
+  });
+});
