3dc2e49ff5a03462296fb9b5dd12eaa1ee93bf58
Ben Gourley <bn@grly.me>
2012-05-23 18:43:42 +0100

    Added test for #11

--- test/compact.test.js
+++ test/compact.test.js
@@ -387,5 +387,34 @@ describe('compact.js', function() {
 
     });
 
+    it('should differentiate between files with the same name from different locations', function (done) {
+
+      var compactDebug = require('../../compact').createCompact({ srcPath: srcPath, destPath: destPath, debug: true });
+
+
+      compactDebug.addNamespace('global')
+        .addJs('/a.js');
+
+        compactDebug.addNamespace('alternative', altPath)
+        .addJs('/a.js');
+
+      var
+        req = {
+          app: {
+            helpers: function(helper) {
+              helper.compactJs()[0].should.not.equal(helper.compactJs()[1]);
+              done();
+            },
+            configure: function(fn) {
+              fn();
+            }
+          }
+        }
+        , res;
+
+      compactDebug.js(['global', 'alternative'])(req, res, function() {});
+
+    });
+
   });
 });
